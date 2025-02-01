154ca372edd98fad795ee33d53f92dfd760652bb
Morgan <58524460+hexhowells@users.noreply.github.com>
2025-02-01 19:23:31 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gym_maze",
+    version="0.1.0",
+    author="Morgan Howells",
+    author_email="hexhowells@gmail.com",
+    description="Gymnasium for randomly generated 3D maze games",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hexhowells/gym-maze",
+    python_requires=">=3.6",
+)
